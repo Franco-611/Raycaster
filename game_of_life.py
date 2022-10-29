b582d758607e6465c7ddc3871812f3c46fd33e72
franco611 <62269078+franco611@users.noreply.github.com>
2022-10-28 22:57:24 -0600

    Final_game_of_life

--- game_of_life.py
+++ game_of_life.py
@@ -30,9 +30,6 @@ def pixeles(matriz, color):
                 glClear(GL_COLOR_BUFFER_BIT)
     glDisable(GL_SCISSOR_TEST)
 
-x = 0
-speed = 1
-
 mat = []
 for i in range(tamaño):
     mat.append([])
@@ -105,9 +102,188 @@ def linea(x,y):
     mat[x+1][y] = 1
     mat[x-1][y] = 1
 
+def movido(x,y):
+    mat[x][y] = 1
+    mat[x][y-2] = 1
+    mat[x+1][y-2] = 1
+    mat[x-1][y-2] = 1
+    mat[x+1][y-1] = 1
+
+def medusa(x,y):
+    mat[x][y] = 1
+    mat[x+1][y] = 1
+    mat[x+2][y] = 1
+    mat[x+3][y] = 1
+    mat[x+4][y] = 1
+    mat[x+4][y-1] = 1
+    mat[x+4][y-2] = 1
+    mat[x+3][y-3] = 1
+    mat[x-1][y-1] = 1
+    mat[x-1][y-3] = 1
+    mat[x+1][y-4] = 1
+
+def casii(x,y):
+    mat[x][y]=1
+    mat[x][y-1]=1
+    mat[x][y-2]=1
+    mat[x-1][y-2]=1
+    mat[x+1][y-2]=1
+
+    mat[x][y-5]=1
+    mat[x+1][y-5]=1
+    mat[x-1][y-5]=1
+    mat[x][y-6]=1
+    mat[x][y-7]=1
+    mat[x][y-8]=1
+    mat[x][y-9]=1
+    mat[x][y-10]=1
+    mat[x+1][y-10]=1
+    mat[x-1][y-10]=1
+
+    mat[x][y-13]=1
+    mat[x-1][y-13]=1
+    mat[x+1][y-13]=1
+    mat[x][y-14]=1
+    mat[x][y-15]=1
+
+def flecha(x,y):
+    mat[x][y]=1
+    mat[x][y-1]=1
+    mat[x-1][y-2]=1
+    mat[x-2][y-2]=1
+    mat[x-3][y-2]=1
+    mat[x-4][y-2]=1
+    mat[x+1][y+1]=1
+    mat[x+1][y+2]=1
+    mat[x+2][y]=1
+    mat[x+3][y+1]=1
+    mat[x+4][y+1]=1
+    mat[x+5][y+1]=1
+
+    mat[x+7][y-1]=1
+    mat[x+8][y-1]=1
+    mat[x+9][y-1]=1
+    mat[x+10][y-1]=1
+    mat[x+6][y-2]=1
+    mat[x+6][y-3]=1
+    mat[x+3][y-3]=1
+    mat[x+1][y-4]=1
+    mat[x+2][y-4]=1
+    mat[x+3][y-4]=1
+    mat[x+5][y-4]=1
+    mat[x+5][y-5]=1
+
+def corazon(x,y):
+
+    mat[x-2][y-2]=1
+    mat[x-2][y-3]=1
+
+    mat[x-1][y-1]=1
+    mat[x-1][y-4]=1
+
+    mat[x][y]=1
+    mat[x][y-3]=1
+
+    mat[x+1][y]=1
+    mat[x+1][y-7]=1
+
+    mat[x+2][y+1]=1
+    mat[x+2][y-2]=1
+    mat[x+2][y-6]=1
+    mat[x+2][y-8]=1
+
+    mat[x+3][y]=1
+    mat[x+3][y-1]=1
+    mat[x+3][y-2]=1
+    mat[x+3][y-6]=1
+    mat[x+3][y-7]=1
+
+    mat[x+5][y]=1
+    mat[x+5][y-1]=1
+    mat[x+5][y-2]=1
+    mat[x+5][y-6]=1
+    mat[x+5][y-7]=1
+
+    mat[x+6][y+1]=1
+    mat[x+6][y-2]=1
+    mat[x+6][y-6]=1
+    mat[x+6][y-8]=1
+
+    mat[x+7][y]=1
+    mat[x+7][y-7]=1
+
+    mat[x+8][y]=1
+    mat[x+8][y-2]=1
+
+    mat[x+9][y-1]=1
+    mat[x+9][y-4]=1
+
+    mat[x+10][y-2]=1
+    mat[x+10][y-3]=1
+
+def generador(x,y):
+    mat[x][y]=1
+    mat[x][y-1]=1
+    mat[x+1][y]=1
+    mat[x+1][y-1]=1
+    
+    mat[x+10][y]=1
+    mat[x+10][y-1]=1
+    mat[x+10][y-2]=1
+
+    mat[x+11][y+1]=1
+    mat[x+11][y-3]=1
+
+    mat[x+12][y+2]=1
+    mat[x+12][y-4]=1
+
+    mat[x+13][y+2]=1
+    mat[x+13][y-4]=1
+
+    mat[x+14][y-1]=1
+
+    mat[x+15][y+1]=1
+    mat[x+15][y-3]=1
+
+    mat[x+16][y]=1
+    mat[x+16][y-1]=1
+    mat[x+16][y-2]=1
+
+    mat[x+17][y-1]=1
+
+    mat[x+20][y]=1
+    mat[x+20][y+1]=1
+    mat[x+20][y+2]=1
+
+    mat[x+21][y]=1
+    mat[x+21][y+1]=1
+    mat[x+21][y+2]=1
+
+    mat[x+22][y+3]=1
+    mat[x+22][y-1]=1
+
+    mat[x+24][y-1]=1
+    mat[x+24][y-2]=1
+    mat[x+24][y+3]=1
+    mat[x+24][y+4]=1
+
+    mat[x+34][y+2]=1
+    mat[x+34][y+1]=1
+    mat[x+35][y+2]=1
+    mat[x+35][y+1]=1
+
+
+
+generador(10,80)
 colmena(10,50)
 bloque(50,60)
 linea(50,10)
+movido(50,80)
+medusa(20,20)
+casii(70,80)
+flecha(20,70)
+corazon(60,60)
+generador(60,10)
 
 verificador()
 
